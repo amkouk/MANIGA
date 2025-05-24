d4b25c65088faf5465a5a4cb40959ad53d6fd466
amkouk <hamzaamkouk10@gmail.com>
2025-05-24 19:31:15 +0200

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,12 @@
+export function renderSidebar() {
+  return `
+    <aside class="sidebar">
+      <ul>
+        <li><a href="#">🏠 الرئيسية</a></li>
+        <li><a href="#">🔍 استكشاف</a></li>
+        <li><a href="#">📩 الرسائل</a></li>
+        <li><a href="#">👤 ملفي</a></li>
+      </ul>
+    </aside>
+  `;
+}
