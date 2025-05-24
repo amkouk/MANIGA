a201934754c955afa10c5c0ec1d69a31297c1ef5
amkouk <hamzaamkouk10@gmail.com>
2025-05-24 19:30:06 +0200

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+export function renderHeader() {
+  return `
+    <header class="header">
+      <div class="logo">MyInsta</div>
+      <input type="text" placeholder="بحث..." class="search-box" />
+      <div class="icons">
+        <i class="icon-home">🏠</i>
+        <i class="icon-message">💬</i>
+        <i class="icon-profile">👤</i>
+      </div>
+    </header>
+  `;
+}
